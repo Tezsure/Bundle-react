c47c5cc377ba625417a038d77799fa10ffd1f6c9
Dibyo Majumder <dibyomajumder23@gmail.com>
2020-06-22 15:40:41 +0530

    smartPy version update

--- utils/scripts/checkScenario.sh
+++ utils/scripts/checkScenario.sh
@@ -1,6 +1,8 @@
-  echo "---------------------";
+echo "---------------------";
 echo " Tests ";
 echo "---------------------";
 ./utils/SmartPyBasic/SmartPy.sh test ./contract/demo_test.py ./test-build; 
-cat ./test-build/test.output;
+echo "Scenario Check :"
 echo "---------------------";
+cat ./test-build/interpreted-scenario/scenario-interpreter-log.txt;
+printf "\n---------------------\n";
