d8fa1470150fabb57e1ba88d295a5dc036f1d712
devansh2712 <devanshswarup@gmail.com>
2020-09-06 15:54:28 +0530

    UI Components

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -8,12 +8,9 @@ function Home() {
       <div class="container">
         <div class="row align-items-center my-5">
           <div class="col-lg-5">
-            <h1 class="font-weight-light">Home</h1>
+            <h1 class="font-weight-light">Decentralised Autonomous Organisation</h1>
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
+              A List of all DAO's Open for Intake
             </p>
           </div>
           <div class="col-lg-3">
